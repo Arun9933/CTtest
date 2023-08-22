f9dd0c91157eb646c7668627dfaa7ab883803aa5
arunmujalge2910@gmail.com <asmujalge333@gmail.com>
2023-08-22 20:20:39 +0530

    CT1

--- src/test/java/CTtest/Test1.java
+++ src/test/java/CTtest/Test1.java
@@ -1,10 +1,11 @@
+package CTtest;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 //import io.github.bonigarcia.wdm.WebDriverManager;
 import org.testng.annotations.Test;
 
-public class CTtest {
+public class Test1 {
 	@Test
 	 void check1() {
 		
