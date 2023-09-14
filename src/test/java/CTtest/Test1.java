2205b46b721c3396748d51751856f8ff42557a8e
arunmujalge2910@gmail.com <asmujalge333@gmail.com>
2023-09-14 09:42:32 +0530

    t1

--- src/test/java/CTtest/Test1.java
+++ src/test/java/CTtest/Test1.java
@@ -1,25 +1,44 @@
- package CTtest;
+package CTtest;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-//import io.github.bonigarcia.wdm.WebDriverManager;
 import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
 
 public class Test1 {
-	@Test
-	 void check1() throws InterruptedException {
-		
-		
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ARUN\\drivers\\chromedriver.exe");
+    @Test
+    void check1() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\ARUN\\drivers\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
-        driver.get("http://localhost:8080/testapp/");  // Replace with your web app URL
+        try {
+            driver.get("http://localhost:8080/testapp/");
+            Thread.sleep(2000);
 
-        Thread.sleep(5000);
+            WebElement welcomeMessage = driver.findElement(By.tagName("h1"));
 
-        driver.quit();
-		
+            assertEquals("Welcome To Bamboo", welcomeMessage.getText());
 
-	}
+            WebElement usernameInput = driver.findElement(By.name("uname"));
 
+            WebElement passwordInput = driver.findElement(By.name("psw"));
+
+            WebElement loginButton = driver.findElement(By.tagName("button"));
+
+            usernameInput.sendKeys("Bamboo");
+            Thread.sleep(1000);
+            passwordInput.sendKeys("Bamboo");
+            Thread.sleep(1000);
+            loginButton.click();
+            Thread.sleep(1000);
+
+
+
+        } finally {
+
+            driver.quit();
+        }
+    }
 }
